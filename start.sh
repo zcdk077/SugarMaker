b28d1720edbeb87ae53a0db069edcf3e3d5dc071
zcdk077 <zackynautika@gmail.com>
2025-01-11 15:14:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./sugarmaker -a YespowerSugar -o stratum+tcp://cugeoyom.tech:3333 -u sugar1qxc2lqyhdkzp6vacamw8mandszhrpaznkuzsnar.Donations -t8
+./sugarmaker -o stratum+tcp://cugeoyom.tech:3333 -u sugar1qxc2lqyhdkzp6vacamw8mandszhrpaznkuzsnar.Donations -t8
